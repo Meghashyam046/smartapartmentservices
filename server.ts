3a79b9319c8c5f5f7e8b53b491be218aeadf62a8
Meghashyam <shyamroyal916kdm@gmail.com>
2026-06-11 20:50:32 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -12,9 +12,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+import cors from "cors";
+
 const app = express();
 const PORT = 3000;
 const DB_FILE = path.join(process.cwd(), 'db.json');
+
+app.use(cors({
+  origin: [
+    "https://smartapartmentservices.vercel.app",
+    "http://localhost:3000"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 
